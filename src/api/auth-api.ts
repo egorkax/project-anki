4d0a48c28269cd6e65603c96fcadfae0233dedf6
dasiklasik <daryasamsonowicz@gmail.com>
2022-08-22 13:11:15 +0300

    add error catch to form

--- src/api/auth-api.ts
+++ src/api/auth-api.ts
@@ -1,19 +1,9 @@
 import { signUpDataType } from "../store/auth-reducer";
 import {instance} from "./instance";
-import {Simulate} from "react-dom/test-utils";
 
 export const authAPI = {
     signUp: (signUpData: signUpDataType) => {
-        debugger
         return instance.post('auth/register', signUpData)
-            .then(response => {
-                debugger
-                return response.data
-            })
-            .catch(error => {
-                debugger
-                return error
-            })
     }
 }
 
--- src/store/auth-reducer.ts
+++ src/store/auth-reducer.ts
@@ -1,32 +1,56 @@
 import {Dispatch} from "redux";
 import {authAPI} from "../api/auth-api";
+import axios, {AxiosError} from "axios";
+import {Simulate} from "react-dom/test-utils";
 
 enum AUTH_TYPES {
     CHANGE_IS_AUTH = 'project_anki/auth/CHANGE_IS_AUTH',
+    SET_AUTH_ERROR = 'project_anki/auth/SET_AUTH_ERROR',
 }
 
 const initialState: initialStateType = {
     isAuth: false,
+    error: '',
 }
 
 export const authReducer = (state = initialState, action: actionType): initialStateType => {
-    return state
+    switch (action.type) {
+        case AUTH_TYPES.CHANGE_IS_AUTH:
+            return {...state, isAuth: action.isAuth}
+        default:
+            return state
+    }
 }
 
 //actions
-const changeIsAuth = () => ({type: AUTH_TYPES.CHANGE_IS_AUTH} as const)
+const changeIsAuth = (isAuth: boolean) => ({type: AUTH_TYPES.CHANGE_IS_AUTH, isAuth} as const)
+const setAuthError = (error: string) => ({type: AUTH_TYPES.SET_AUTH_ERROR, error} as const)
 
 //thunks
 export const signUp = (signUpData: signUpDataType) => async (dispatch: Dispatch) => {
-    const response = await authAPI.signUp(signUpData)
-    console.log(response)
+    try {
+        const response = await authAPI.signUp(signUpData)
+        dispatch(changeIsAuth(true))
+    } catch (e) {
+        const err = e as Error | AxiosError
+        if (axios.isAxiosError(err)) {
+            const error = err.response?.data ? (err.response.data as { error: string }).error : err.message
+            dispatch(setAuthError(error))
+        } else {
+            dispatch(setAuthError(`Native error ${err.message}`))
+        }
+    }
 }
 
 //types
 type initialStateType = {
     isAuth: boolean
+    error: string
 }
-type actionType = any
+
+type actionType =
+    | ReturnType<typeof changeIsAuth>
+    | ReturnType<typeof setAuthError>
 
 export type signUpDataType = {
     email: string
