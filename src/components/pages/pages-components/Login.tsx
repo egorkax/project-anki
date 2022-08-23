4659ce52bc76b39ed28fdea10bf95a48ae03bf01
Angor78 <angor78@gmail.com>
2022-08-23 09:20:32 +0300

    add className for Login page from auth.css

--- src/components/pages/pages-components/Login.tsx
+++ src/components/pages/pages-components/Login.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import {SignInForm} from "../../SignInForm/SignInForm";
+import {Navigate, NavLink} from "react-router-dom";
+import {useSelector} from "react-redux";
+import {AppRootStateType} from "../../../store/store";
 
 export const Login = () => {
+  const isAuth = useSelector<AppRootStateType, boolean>(state => state.signIn.isAuth)
+  if(isAuth){
+    return <Navigate to={'/profile'}/>
+  }
   return (
-    <div className="">
+    <div className="auth-container">
       <h1>Sign in</h1>
       <SignInForm/>
+      <div className='add-reg-block'>
+        <p>Already have an account?</p>
+        <NavLink className='underlinedLink' to={'/registration'}>Sign Up</NavLink>
+      </div>
     </div>
   );
 }
-
