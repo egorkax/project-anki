b4a7f3afe58f4e955ac3681e2b219197d34ead35
Angor78 <angor78@gmail.com>
2022-08-23 19:09:10 +0300

    add recovery reducer to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,11 +3,13 @@ import thunkMiddleware from 'redux-thunk'
 import {TypedUseSelectorHook, useSelector} from "react-redux";
 import {generalReducer} from "./general-reducer";
 import {signInReducer} from "./signIn-reducer";
+import {recoveryPassReducer} from "./recoveryPass-reducer";
 
 
 const rootReducer = combineReducers({
   data: generalReducer,
   signIn: signInReducer,
+  recovery: recoveryPassReducer
 })
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
