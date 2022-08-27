8d788fd8b6bbdc82a7ffa733c6bcb389d25853db
Angor78 <angor78@gmail.com>
2022-08-27 13:52:11 +0300

    use useAppSelector into Profile

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import s from './Profile.module.css'
 import {Navigate, NavLink} from "react-router-dom";
-import {useSelector} from "react-redux";
-import {AppRootStateType} from "../../store/store";
+import {useAppSelector} from "../../store/store";
 import {ProfileInfo} from "./ProfileInfo/ProfileInfo";
 import arrow from '../../assets/images/arrow.svg'
 
 
 export const Profile = () => {
 
-    const isAuth = useSelector<AppRootStateType, boolean>(state => state.auth.isAuth)
+    const isAuth = useAppSelector(state => state.auth.isAuth)
 
     if(!isAuth) return <Navigate  to={'/login'}/>
-
-
-
 
     return (
         <div className={s.blockProfile}>
