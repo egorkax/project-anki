52a264923813806cfc26d826f8f2edcf3be53d90
Angor78 <angor78@gmail.com>
2022-08-27 14:00:51 +0300

    use useAppSelector into RecoveryPassword

--- src/components/Auth/RecoveryPassword.tsx
+++ src/components/Auth/RecoveryPassword.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-
-import {useSelector} from "react-redux";
-import {AppRootStateType} from "../../store/store";
+import {useAppSelector} from "../../store/store";
 import {CheckEmail} from "./CheckEmail";
 import {ForgotPassword} from "./ForgotPassword";
 
 export const RecoveryPassword = () => {
-    const isSentData = useSelector<AppRootStateType, boolean>(state => state.auth.isSentData)
+
+  const isSentData = useAppSelector(state => state.auth.isSentData)
+
     if (!isSentData) {
         return <ForgotPassword/>
     } else {
