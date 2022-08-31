febb7e49c5c52ec4be9bec810c7ea9a728a88669
Angor78 <angor78@gmail.com>
2022-08-31 13:24:38 +0300

    fix icons

--- src/components/Auth/CheckEmail.tsx
+++ src/components/Auth/CheckEmail.tsx
@@ -3,7 +3,7 @@ import {NavLink} from "react-router-dom";
 import {setRecoveryStatus} from "../../reducers/auth-reducer";
 import SuperButton from "../../common/SuperButton/SuperButton";
 import {useAppDispatch, useAppSelector} from "../../store/store";
-import {FilterSvgIcon} from "../../assets/icons/MailSvgIcon/FilterSvgIcon";
+import {MailSvgIcon} from "../../assets/icons/MailSvgIcon";
 
 
 export const CheckEmail = () => {
@@ -20,7 +20,7 @@ export const CheckEmail = () => {
       <h1>Check Email</h1>
       <div className='add-reg-block'>
         <div>
-          <FilterSvgIcon/>
+          <MailSvgIcon/>
         </div>
         <div className='auth-desc'>We’ve sent an Email with instructions to {recoveryEmail}</div>
         <div className='auth-button-wrapper'>
