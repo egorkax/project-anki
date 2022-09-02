b57576aef9a20bf5e16f4ac753749f7fe86a484b
dasiklasik <daryasamsonowicz@gmail.com>
2022-09-02 19:52:56 +0300

    fix header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,33 @@
 import React from 'react'
-import {NavLink} from "react-router-dom";
+import {NavLink, useLocation} from "react-router-dom";
 import s from './Header.module.css'
 import logo from '../../assets/images/logo.svg'
 import SuperButton from "../../common/SuperButton/SuperButton";
 import {useAppSelector} from "../../store/store";
-import {HeaderProfile} from "../Header/HeaderProfile";
+import { DropDownMenu } from './HeaderProfile/DropDownMenu';
+import { LinearLoader } from '../../common/Loaders/LinearLoader/LinearLoader';
 
 export const Header = () => {
 
-  const isAuth = useAppSelector(state => state.auth.isAuth)
+    const appStatus = useAppSelector(state => state.app.appStatus)
+    const isAuth = useAppSelector(state => state.auth.isAuth)
 
-  return (
-    <header className={s.header}>
-      <div className={s.wrapper}>
-        <img src={logo} alt=''/>
-        {!isAuth ?
-          <SuperButton><NavLink to={'/login'}>Sign in</NavLink></SuperButton>
-          : <HeaderProfile/>
-        }
-      </div>
-    </header>
-  )
+    const isLoading = appStatus === "loading"
+
+    const {pathname} = useLocation()
+
+    return (
+        <header className={s.header}>
+            <div className={s.wrapper}>
+                <img src={logo} alt=''/>
+                {isAuth ?
+                    <DropDownMenu/> :
+                    pathname === '/login' ? <SuperButton><NavLink to={'/registration'}>Sign up</NavLink></SuperButton>
+                        : <SuperButton><NavLink to={'/login'}>Sign in</NavLink></SuperButton>
+                }
+            </div>
+            {isLoading ?  <LinearLoader isLoading={isLoading}/> : null}
+        </header>
+    )
 };
 
