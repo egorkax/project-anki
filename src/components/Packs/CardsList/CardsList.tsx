876f6598cbc4b47fcc71d38411897173c25742c7
dasiklasik <daryasamsonowicz@gmail.com>
2022-09-02 20:04:22 +0300

    fix CardsList.tsx

--- src/components/Packs/CardsList/CardsList.tsx
+++ src/components/Packs/CardsList/CardsList.tsx
@@ -1,91 +1,102 @@
 import React, {useEffect} from "react";
 import SuperButton from "../../../common/SuperButton/SuperButton";
-import {Navigate, useParams} from "react-router-dom";
+import {Navigate, NavLink, useParams} from "react-router-dom";
 import {useDispatch} from "react-redux";
 import {ThunkDispatch} from "redux-thunk";
 import {AppRootStateType, useAppSelector} from "../../../store/store";
 import {AnyAction} from "redux";
 import {
-  createCard,
-  changeFilterCardQuestion,
-  currentCardsPage,
-  fetchCards,
-  showCardsPerPage
+    createCard,
+    changeFilterCardQuestion,
+    currentCardsPage,
+    fetchCards,
+    showCardsPerPage
 } from "../../../reducers/cards-reducer";
 import {BackToPacksLink} from "../../../common/BackToPacksLink/BackToPacksLink";
 import '../PacksBlock.css'
 import {CardsTable} from "./CardsTable";
 import {SearchField} from "../../Search&Filter/SearchField/SearchField";
 import {Paginator} from "../../../common/Paginator/Paginator";
+import style from "../PacksList/PacksTable/PacksTableItem/PacksTableItem.module.css";
+import {LearnSvgIcon} from "../../../assets/icons/LearnSvgIcon";
 
 
 export const CardsList = () => {
 
-  const {packId} = useParams()
-  const dispatch = useDispatch<ThunkDispatch<AppRootStateType, void, AnyAction>>()
+    const {packId} = useParams()
+    const dispatch = useDispatch<ThunkDispatch<AppRootStateType, void, AnyAction>>()
 
-  const packName = useAppSelector(state => state.cards.packName)
-  const isAuth = useAppSelector(state => state.auth.isAuth)
-  const cardsCount = useAppSelector(state => state.cards.cardsTotalCount)
-  const totalCardsCount = useAppSelector(state => state.cards.cardsTotalCount)
-  const cardsPageCount = useAppSelector(state => state.cards.pageCount)
-  const cardsPage = useAppSelector(state => state.cards.page)
+    const packName = useAppSelector(state => state.cards.packName)
+    const isAuth = useAppSelector(state => state.auth.isAuth)
+    const cardsCount = useAppSelector(state => state.cards.cardsTotalCount)
+    const totalCardsCount = useAppSelector(state => state.cards.cardsTotalCount)
+    const cardsPageCount = useAppSelector(state => state.cards.pageCount)
+    const cardsPage = useAppSelector(state => state.cards.page)
+    const userPackId = useAppSelector(state => state.cards.packUserId)
+    const userId = useAppSelector(state => state.profile._id)
 
-  useEffect(() => {
-    if (packId && isAuth) {
-      dispatch(fetchCards(packId))
+    const isMy = userPackId === userId
+
+    useEffect(() => {
+        if (packId && isAuth) {
+            dispatch(fetchCards(packId))
+        }
+    })
+
+    if (!isAuth) return <Navigate to={'/login'}/>
+
+    const searchCards = (value: string) => {
+        dispatch(changeFilterCardQuestion(value))
+        if (packId) {
+            dispatch(fetchCards(packId))
+        }
     }
-  })
 
-  if (!isAuth) return <Navigate to={'/login'}/>
-
-  const searchCards = (value: string) => {
-    dispatch(changeFilterCardQuestion(value))
-    if (packId) {
-      dispatch(fetchCards(packId))
+    const addCard = () => {
+        if (packId) {
+            dispatch(createCard(packId))
+        }
     }
-  }
 
-  const addCard = () => {
-    if (packId) {
-      dispatch(createCard(packId))
+    const onChangeShowItems = (pageCount: number) => {
+        if (packId) {
+            dispatch(showCardsPerPage(pageCount, packId))
+        }
     }
-  }
-
-  const onChangeShowItems = (pageCount: number) => {
-    if (packId) {
-      dispatch(showCardsPerPage(pageCount, packId))
+    const onChangePageHandler = (page: number) => {
+        if (packId) {
+            dispatch(currentCardsPage(page, packId))
+        }
     }
-  }
-  const onChangePageHandler = (page: number) => {
-    if (packId) {
-      dispatch(currentCardsPage(page, packId))
-    }
-  }
 
-  if (!isAuth) return <Navigate to={'/login'}/>
+    if (!isAuth) return <Navigate to={'/login'}/>
 
-  return (
-    <div className='packs-wrapper'>
-      <BackToPacksLink/>
-      {cardsCount === 0 ?
-        <div className='empty-cards-block'>
-          <p>This pack is empty. Click add new card to fill this pack</p>
-          <SuperButton onClick={addCard}>Add new card</SuperButton>
+    return (
+        <div className='packs-wrapper'>
+            <BackToPacksLink/>
+            {cardsCount !== 0 ?
+                <div className='packs-block'>
+                    <div className='packs-header'>
+                        <h1>{packName}</h1>
+                        {isMy ? <SuperButton onClick={addCard}>Add new card</SuperButton>
+                            : <SuperButton><NavLink to={`/packs/learn/${packId}`} className={style.icon}>Learn pack</NavLink></SuperButton>}
+                    </div>
+                    <SearchField searchFunction={searchCards}/>
+                    <CardsTable packId={packId}/>
+                    <Paginator totalItemsCount={totalCardsCount}
+                               pageSize={cardsPageCount}
+                               currentPage={cardsPage}
+                               onChangePage={onChangePageHandler}
+                               onChangeShowItems={onChangeShowItems}/>
+                </div>
+                : isMy ?
+                    <div className='empty-cards-block'>
+                        <p>This pack is empty. Click add new card to fill this pack</p>
+                        <SuperButton onClick={addCard}>Add new card</SuperButton>
+                    </div>
+                    : <div className='empty-cards-block'>
+                        <p>This pack is empty.</p>
+                    </div>}
         </div>
-        : <div className='packs-block'>
-          <div className='packs-header'>
-            <h1>{packName}</h1>
-            <SuperButton onClick={addCard}>Add new card</SuperButton>
-          </div>
-          <SearchField searchFunction={searchCards}/>
-          <CardsTable packId={packId}/>
-          <Paginator totalItemsCount={totalCardsCount}
-                     pageSize={cardsPageCount}
-                     currentPage={cardsPage}
-                     onChangePage={onChangePageHandler}
-                     onChangeShowItems={onChangeShowItems}/>
-        </div>}
-    </div>
-  )
+    )
 }
