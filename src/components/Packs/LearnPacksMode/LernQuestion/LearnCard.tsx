2bc737b5f0437bddbf22ec83683c7e4859189124
Angor78 <angor78@gmail.com>
2022-09-07 15:13:00 +0300

    refactor styles Learn

--- src/components/Packs/LearnPacksMode/LernQuestion/LearnCard.tsx
+++ src/components/Packs/LearnPacksMode/LernQuestion/LearnCard.tsx
@@ -4,26 +4,26 @@ import {CardType} from "../../../../api/cards-api";
 import s from './LearnCard.module.css'
 
 type PropsType = {
-    buttonName: string;
-    card: CardType;
-    onClick: () => void;
-    isChecked: boolean
-    children?: ReactNode;
+  buttonName: string;
+  card: CardType;
+  onClick: () => void;
+  isChecked: boolean
+  children?: ReactNode;
 };
 export const LearnCard = (props: PropsType) => {
-    const {buttonName, card, onClick, isChecked, children} = props
-    return (
-        <div className={!isChecked ? s.blockLearn : s.blockLearnAnswer}>
-            <div className={s.question}>
-                <b>Question:</b> {card.question}
-            </div>
-            <div className={s.attempts}>
-                Number of attempts to answer the question: <b>{card.shots}</b>
-            </div>
-            {children}
-            <SuperButton className={s.button} onClick={onClick}>{buttonName}</SuperButton>
-        </div>
+  const {buttonName, card, onClick, isChecked, children} = props
+  return (
+    <div className={!isChecked ? s.blockLearn : s.blockLearnAnswer}>
+      <div className={s.question}>
+        <b>Question:</b> {card.question}
+      </div>
+      <div className={s.attempts}>
+        Number of attempts to answer the question: <b>{card.shots}</b>
+      </div>
+      {children}
+      <SuperButton className={s.button} onClick={onClick}>{buttonName}</SuperButton>
+    </div>
 
-    )
-        ;
+  )
+    ;
 };
